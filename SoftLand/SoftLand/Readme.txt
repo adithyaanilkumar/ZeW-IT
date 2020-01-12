825d67b290856ac6eaa370e475436adcaeeb2660
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-01-12 22:23:22 +0530

    initial commit

---
+++ SoftLand/SoftLand/Readme.txt
@@ -0,0 +1,6 @@
+Thanks for downloading this template!
+
+Template Name: SoftLand
+Template URL: https://bootstrapmade.com/softland-bootstrap-app-landing-page-template/
+Author: BootstrapMade.com
+Author URL: https://bootstrapmade.com/
---
+++ landing/src/Components/Header.js
@@ -0,0 +1,21 @@
+import react, {component} from React
+import ReactDOM from 'react-dom'
+import { Component } from 'react'
+import '../CSS/Header.CSS'
+
+class header extends Component(){
+    render(){
+        <div className="header">
+            <div class="menu">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Product</li>
+                    <li>contact</li>
+                </ul>
+            </div>
+        </div>
+        
+    }
+}
